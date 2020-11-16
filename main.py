83bdf3c875aedcf6edfa2095d8fe09dbdde69969
botan <botan.david94@gmail.com>
2020-11-16 19:31:56 +0100

    Include buy/sell with strategy

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 from binance.client import Client
 from binance.enums import *
+from binance.exceptions import BinanceAPIException
 from binance.websockets import BinanceSocketManager
 import pandas as pd
 import numpy as np
@@ -9,6 +10,7 @@ from datetime import datetime
 
 from ta.momentum import RSIIndicator
 from ta.trend import EMAIndicator
+import json
 
 TREND_RANGE = 0
 TREND_BULLISH = 1
@@ -18,11 +20,24 @@ TRENDS_NAME = ["RANGE", "BULLISH", "BEARISH"]
 
 
 def is_crossing_down(df, first_key, second_key):
-    return df[first_key].iloc[-1] > df[second_key].iloc[-1] and df[first_key].iloc[-2] <= df[second_key].iloc[-2]
+    return df[first_key].iloc[-1] < df[second_key].iloc[-1] \
+           and df[first_key].iloc[-2] >= df[second_key].iloc[-2]
 
 
 def is_crossing_up(df, first_key, second_key):
-    return df[first_key].iloc[-1] > df[second_key].iloc[-1] and df[first_key].iloc[-2] <= df[second_key].iloc[-2]
+    return df[first_key].iloc[-1] > df[second_key].iloc[-1] \
+           and df[first_key].iloc[-2] <= df[second_key].iloc[-2]
+
+
+def save_order(order, filename):
+    json.dump(order, open(filename, 'w'))
+
+
+def read_order(filename):
+    return json.load(open(filename, 'w'))
+
+
+order_filename = "last_order.json"
 
 
 class BinanceBot:
@@ -33,27 +48,51 @@ class BinanceBot:
         self.symbol = symbol
         self.interval = interval
         self.trend = TREND_RANGE
-        self.data = pd.DataFrame()
+        self.data: pd.DataFrame = pd.DataFrame()
         self.websocket.start()
+        self.quantity = 10
+        self.lastOrder = read_order(order_filename)
 
     def start(self):
-        print(F"Initializing bot on {self.symbol} with TF: {self.interval} [{datetime.now()}]")
+        print(F"Initializing bot on {self.symbol} with TF: {self.interval} [{datetime.now()}]\n")
+        self.update_account_balance()
+        self.cancel_open_orders()
         self.initialize_candles()
-        self.define_trend()
         self.start_websocket()
-        self.on_update()
+
+    def cancel_open_orders(self):
+        open_orders = self.client.get_open_orders(symbol=self.symbol)
+        for order in open_orders:
+            self.client.cancel_order(symbol=self.symbol, clientId=order['clientId'])
+        if len(open_orders) > 0:
+            print(F"Cancel {len(open_orders)} orders")
 
     def on_update(self):
-        if is_crossing_up(self.data, "ema_8", "ema_21"):
-            print(F"{datetime.now()} EMA IS CROSSING UP!")
-        elif is_crossing_down(self.data, "ema_8", "ema_21"):
-            print(F"{datetime.now()} EMA IS CROSSING DOWN!")
+        try:
+            if is_crossing_up(self.data, "ema_8", "ema_21"):
+                self.lastOrder = self.client.create_order(symbol=self.symbol, side=SIDE_BUY, type=ORDER_TYPE_MARKET,
+                                                          quoteOrderQty=self.quantity,
+                                                          newOrderRespType=ORDER_RESP_TYPE_FULL)
+                save_order(self.lastOrder, order_filename)
+                print(F"{datetime.now()}: buying {self.lastOrder}")
+            elif is_crossing_down(self.data, "ema_8", "ema_21") and self.lastOrder is not None:
+                sell_order = self.client.create_order(symbol=self.symbol, side=SIDE_SELL, type=ORDER_TYPE_MARKET,
+                                                      quantity=self.lastOrder.executedQty)
+                print(F"{datetime.now()} selling position {sell_order}")
+                self.lastOrder = None
+                self.update_account_balance()
+        except BinanceAPIException as e:
+            print("Error: ", e)
+
+    def update_account_balance(self):
+        self.quantity = self.client.get_asset_balance("USDT")['free']
+        print(F"Account balance: {self.quantity}$")
 
     def define_trend(self):
         trend_line = self.data["ema_55"].iloc[-1].item()
         current_price = self.data["close"].iloc[-1].item()
         percent = ((abs(trend_line - current_price) / current_price) * 100)
-        if percent < 1.0:
+        if percent < 0.3:
             self.trend = TREND_RANGE
         elif trend_line > current_price:
             self.trend = TREND_BEARISH
@@ -80,6 +119,7 @@ class BinanceBot:
         self.data['volume'] = np.array(volumes).astype(np.float)
         self.calculate_ta()
         print(F"Successfully loaded {len(self.data)} candles")
+        self.define_trend()
 
     def calculate_ta(self):
         self.data['rsi'] = RSIIndicator(close=self.data["close"], n=14).rsi()
@@ -99,6 +139,7 @@ class BinanceBot:
             self.data = self.data.append(df, ignore_index=True)
             self.calculate_ta()
             self.define_trend()
+            self.on_update()
 
     def start_websocket(self):
         self.websocket.start_kline_socket(self.symbol, self.kline_callback, self.interval)
